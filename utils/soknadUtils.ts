259e2f993408a078e69bd6573d707692a0cd247b
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-30 08:51:27 +0100

    oppretting av ny soknad ved send og bekreft av sykmelding

--- utils/soknadUtils.ts
+++ utils/soknadUtils.ts
@@ -1,19 +1,33 @@
+import dayjs from "dayjs";
+import uuid from "uuid";
+
 import { RSSoknadstatus } from "../types/soknadTypes/rs-types/rs-soknadstatus";
 import { generiskSoknad } from "../data/soknader/generiskSoknad";
 import { Soknad } from "../types/soknadTypes/soknadTypes";
+import { StatusTyper } from "../types/sykmeldingTypes";
 
 export const genererNySoknad = (
   sykmeldingId: string,
-  soknadStatus: RSSoknadstatus,
-  fom: string,
-  tom: string
+  sykmeldingFom: Date,
+  sykmeldingTom: Date,
+  sykmeldingSendtEllerBekreftet?: StatusTyper
 ): Soknad => {
+  const erIFremTiden = dayjs(sykmeldingTom).isAfter(dayjs(new Date()));
+  let soknadstatus: RSSoknadstatus = RSSoknadstatus.NY;
+
+  if (erIFremTiden) {
+    soknadstatus = RSSoknadstatus.FREMTIDIG;
+  } else if (sykmeldingSendtEllerBekreftet) {
+    soknadstatus = RSSoknadstatus.SENDT;
+  }
+
   const soknad: Soknad = new Soknad({
     ...generiskSoknad,
+    id: uuid(),
     sykmeldingId: sykmeldingId,
-    status: soknadStatus,
-    fom: fom,
-    tom: tom
+    status: soknadstatus,
+    fom: dayjs(sykmeldingFom).format("YYYY-MM-DD"),
+    tom: dayjs(sykmeldingTom).format("YYYY-MM-DD")
   });
   return soknad;
 };
