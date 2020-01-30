efe4ea9cbfcd4129f1d3143237b40869047483de
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-01-30 10:18:40 +0100

    legger til nye sykmeldinger

--- data/sykmeldinger.ts
+++ data/sykmeldinger.ts
@@ -1,26 +1,44 @@
+import { nySykmeldingVariasjon1Sykmelding } from "./brukerreiser/steg1/variasjon1/sykmeldinger/ny-sykmelding-variasjon-1-sykmelding";
 import { ferdigSykmeldingVariasjon1SykmeldingA } from "./brukerreiser/steg1/variasjon1/sykmeldinger/ferdig-sykmelding-variasjon-1-sykmelding-a";
 import { ferdigSykmeldingVariasjon1SykmeldingB } from "./brukerreiser/steg1/variasjon1/sykmeldinger/ferdig-sykmelding-variasjon-1-sykmelding-b";
 import { ferdigSykmeldingVariasjon1SykmeldingC } from "./brukerreiser/steg1/variasjon1/sykmeldinger/ferdig-sykmelding-variasjon-1-sykmelding-c";
 
+import { nySykmeldingVariasjon2SykmeldingA } from "./brukerreiser/steg1/variasjon2/sykmeldinger/ny-sykmelding-variasjon-2-sykmelding-a";
+import { nySykmeldingVariasjon2SykmeldingB } from "./brukerreiser/steg1/variasjon2/sykmeldinger/ny-sykmelding-variasjon-2-sykmelding-b";
 import { ferdigSykmeldingVariasjon2SykmeldingA } from "./brukerreiser/steg1/variasjon2/sykmeldinger/ferdig-sykmelding-variasjon-2-sykmelding-a";
 import { ferdigSykmeldingVariasjon2SykmeldingB } from "./brukerreiser/steg1/variasjon2/sykmeldinger/ferdig-sykmelding-variasjon-2-sykmelding-b";
 import { ferdigSykmeldingVariasjon2SykmeldingC } from "./brukerreiser/steg1/variasjon2/sykmeldinger/ferdig-sykmelding-variasjon-2-sykmelding-c";
 
+import { nySykmeldingVariasjon3SykmeldingA } from "./brukerreiser/steg1/variasjon3/sykmeldinger/ny-sykmelding-variasjon-3-sykmelding-a";
+import { nySykmeldingVariasjon3SykmeldingB } from "./brukerreiser/steg1/variasjon3/sykmeldinger/ny-sykmelding-variasjon-3-sykmelding-b";
 import { ferdigSykmeldingVariasjon3SykmeldingA } from "./brukerreiser/steg1/variasjon3/sykmeldinger/ferdig-sykmelding-variasjon-3-sykmelding-a";
 import { ferdigSykmeldingVariasjon3SykmeldingB } from "./brukerreiser/steg1/variasjon3/sykmeldinger/ferdig-sykmelding-variasjon-3-sykmelding-b";
 
+import { nySykmeldingVariasjon4SykmeldingA } from "./brukerreiser/steg1/variasjon4/sykmeldinger/ny-sykmelding-variasjon-4-sykmelding-a";
+import { nySykmeldingVariasjon4SykmeldingB } from "./brukerreiser/steg1/variasjon4/sykmeldinger/ny-sykmelding-variasjon-4-sykmelding-b";
+import { nySykmeldingVariasjon4SykmeldingC } from "./brukerreiser/steg1/variasjon4/sykmeldinger/ny-sykmelding-variasjon-4-sykmelding-c";
+import { nySykmeldingVariasjon4SykmeldingD } from "./brukerreiser/steg1/variasjon4/sykmeldinger/ny-sykmelding-variasjon-4-sykmelding-d";
 import { ferdigSykmeldingVariasjon4SykmeldingA } from "./brukerreiser/steg1/variasjon4/sykmeldinger/ferdig-sykmelding-variasjon-4-sykmelding-a";
 import { ferdigSykmeldingVariasjon4SykmeldingB } from "./brukerreiser/steg1/variasjon4/sykmeldinger/ferdig-sykmelding-variasjon-4-sykmelding-b";
 
 export const sykmeldinger = [
+  nySykmeldingVariasjon1Sykmelding,
   ferdigSykmeldingVariasjon1SykmeldingA,
   ferdigSykmeldingVariasjon1SykmeldingB,
   ferdigSykmeldingVariasjon1SykmeldingC,
+  nySykmeldingVariasjon2SykmeldingA,
+  nySykmeldingVariasjon2SykmeldingB,
   ferdigSykmeldingVariasjon2SykmeldingA,
   ferdigSykmeldingVariasjon2SykmeldingB,
   ferdigSykmeldingVariasjon2SykmeldingC,
+  nySykmeldingVariasjon3SykmeldingA,
+  nySykmeldingVariasjon3SykmeldingB,
   ferdigSykmeldingVariasjon3SykmeldingA,
   ferdigSykmeldingVariasjon3SykmeldingB,
+  nySykmeldingVariasjon4SykmeldingA,
+  nySykmeldingVariasjon4SykmeldingB,
+  nySykmeldingVariasjon4SykmeldingC,
+  nySykmeldingVariasjon4SykmeldingD,
   ferdigSykmeldingVariasjon4SykmeldingA,
   ferdigSykmeldingVariasjon4SykmeldingB
 ];
